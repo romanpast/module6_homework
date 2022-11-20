fd807446d09bceab391769fa79aa5b480e59fafe
romanpast <pastroman98@gmail.com>
2022-11-21 00:28:23 +0900

    done

--- module6.5-task3.js
+++ module6.5-task3.js
@@ -1,17 +1,7 @@
-const isPrime = (num) => {
-    let prime = 'prime',
-        composite = 'composite',
-        error = 'Error: incorrect input, should be less than 1000',
-        one = '1 is neither prime nor composite number.';
-    if (num === 1) return one;
-    if (num > 1000) return error;
-    if (num <= 1 && num > 1000) return composite;
-    if (num == 2) return prime;
-    for (let i = 2; i <= num / 2; i++) {
-        if (num % i == 0) {
-            return composite;
-        }
+function sum(a) {
+    return function (b) {
+        return a + b;
     }
-    return prime;
 }
 
+console.log(sum(10)(5));
